0bcb6e493fc3ac55929a24fefac0561e78e7a727
Kevin Harrington <mad.hephaestus@gmail.com>
2025-07-21 17:26:41 -0400

    remove the external resolution

--- src/test/java/eu/mihosoft/vrl/v3d/SvgExportTest.java
+++ src/test/java/eu/mihosoft/vrl/v3d/SvgExportTest.java
@@ -120,7 +120,7 @@ public class SvgExportTest {
 
       File defaultDir = new File("svg/SVGExportTest6.svg");
       SVGLoad s = new SVGLoad(defaultDir.toURI());
-      ArrayList<CSG>gear = s.extrude(10,0.001);
+      ArrayList<CSG>gear = s.extrude(10);
       //System.out.println("SVG Elements ="+gear);
       
   }   	
