58665dcf0310d6bdb59134dd100948396303c0df
Kevin Harrington <mad.hephaestus@gmail.com>
2026-03-05 14:19:57 -0500

    updating to the latest version from clojar

--- src/test/java/eu/mihosoft/vrl/v3d/Manifold3d_test.java
+++ src/test/java/eu/mihosoft/vrl/v3d/Manifold3d_test.java
@@ -6,6 +6,6 @@ import manifold3d.Manifold;
 public class Manifold3d_test {
 	@Test
 	public void loadTest() {
-		Manifold sphere = Manifold3d.Sphere(10.0f, 20);
+		Manifold sphere = Manifold.Sphere(10.0f, 20);
 	}
 }
