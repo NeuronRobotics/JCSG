808d22336a1813cfaf5ee2e6bcde52c959c36c30
Kevin Harrington <mad.hephaestus@gmail.com>
2026-03-05 14:06:14 -0500

    basic build test

---
+++ src/test/java/eu/mihosoft/vrl/v3d/Manifold3d_test.java
@@ -0,0 +1,11 @@
+package eu.mihosoft.vrl.v3d;
+
+import org.junit.Test;
+import manifold3d.Manifold;
+
+public class Manifold3d_test {
+	@Test
+	public void loadTest() {
+		Manifold sphere = Manifold3d.Sphere(10.0f, 20);
+	}
+}
