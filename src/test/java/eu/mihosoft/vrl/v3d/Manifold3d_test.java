c9736d34955f1ac3e6a067ab9e176bf2cac45ca5
Kevin Harrington <mad.hephaestus@gmail.com>
2026-03-15 17:41:21 -0400

    updaing to the lates CI build release

--- src/test/java/eu/mihosoft/vrl/v3d/Manifold3d_test.java
+++ src/test/java/eu/mihosoft/vrl/v3d/Manifold3d_test.java
@@ -1,13 +1,15 @@
 package eu.mihosoft.vrl.v3d;
 
+import java.lang.foreign.MemorySegment;
+
 import org.junit.Test;
 
 import com.cadoodlecad.manifold.ManifoldBindings;
 
 public class Manifold3d_test {
 	@Test
-	public void loadTest() throws Exception {
+	public void loadTest() throws Throwable {
 		ManifoldBindings manifold = new ManifoldBindings();
-		
+		MemorySegment cube = manifold.cube(10, 10, 10, false);
 	}
 }
