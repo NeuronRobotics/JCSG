742685efd7b8742c2dd83e4c1309931d53186667
Kevin Harrington <mad.hephaestus@gmail.com>
2026-04-10 08:36:35 -0400

    error for non-manifold

---
+++ src/main/java/com/neuronrobotics/manifold3d/NonManifoldShapeError.java
@@ -0,0 +1,11 @@
+package com.neuronrobotics.manifold3d;
+
+public class NonManifoldShapeError extends Exception {
+
+	private static final long serialVersionUID = 9171271069943603762L;
+
+	public NonManifoldShapeError(String string) {
+		super(string);
+	}
+
+}
