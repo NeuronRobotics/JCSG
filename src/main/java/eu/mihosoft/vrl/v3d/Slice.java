76e287e78178760eac6b4f94ef571e190fa9f81d
Kevin Harrington <mad.hephaestus@gmail.com>
2026-05-03 20:05:42 -0400

    formatting

--- src/main/java/eu/mihosoft/vrl/v3d/Slice.java
+++ src/main/java/eu/mihosoft/vrl/v3d/Slice.java
@@ -1,7 +1,6 @@
 package eu.mihosoft.vrl.v3d;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import javafx.scene.image.WritableImage;
@@ -477,18 +476,18 @@ public class Slice {
 	}
 
 	private static List<Polygon> sanatize(List<Polygon> slice) throws ColinearPointsException {
-//		for (int i = 0; i < slice.size(); i++) {
-//			Polygon me = slice.get(i);
-//			boolean bad = !Extrude.isCCW(me);
-//			if (bad) {
-//				// println "Bad polygon!"
-//				List<Vector3d> points = me.getPoints();
-//				ArrayList<Vector3d> result = new ArrayList<Vector3d>(points);
-//				Collections.reverse(result);
-//				me = Polygon.fromPoints(result);
-//			}
-//			slice.set(i, me);
-//		}
+		// for (int i = 0; i < slice.size(); i++) {
+		// Polygon me = slice.get(i);
+		// boolean bad = !Extrude.isCCW(me);
+		// if (bad) {
+		// // println "Bad polygon!"
+		// List<Vector3d> points = me.getPoints();
+		// ArrayList<Vector3d> result = new ArrayList<Vector3d>(points);
+		// Collections.reverse(result);
+		// me = Polygon.fromPoints(result);
+		// }
+		// slice.set(i, me);
+		// }
 		return slice;
 	}
 
