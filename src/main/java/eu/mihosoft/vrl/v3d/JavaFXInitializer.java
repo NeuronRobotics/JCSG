e3fd1e142111b1c54b7a85f6430a31ddc039d894
Kevin Harrington <mad.hephaestus@gmail.com>
2026-06-24 13:31:31 -0400

    have initializer but do not use it

--- src/main/java/eu/mihosoft/vrl/v3d/JavaFXInitializer.java
+++ src/main/java/eu/mihosoft/vrl/v3d/JavaFXInitializer.java
@@ -21,37 +21,36 @@ public class JavaFXInitializer {
 		}
 		latch = true;
 	}
-	// public static void go() {
-	// if (latch) {
-	// // System.out.println("ERR initializer already started");
-	// return;
-	// }
-	// new Thread() {
-	// public void run() {
-	// try {
-	// gointernal();
-	// } catch (Throwable t) {
-	// t.printStackTrace();
-	// errored = true;
-	// }
-	// }
-	// }.start();
-	// try {
-	// long start = System.currentTimeMillis();
-	// while ((System.currentTimeMillis() - start) < 1000 && latch == false) {
-	// Thread.sleep(16);
-	// }
-	// if (!latch)
-	// errored = true;
-	// } catch (Throwable e) {
-	// e.printStackTrace();
-	// errored = true;
-	// }
-	// StackTraceElement[] stacktrace = Thread.currentThread().getStackTrace();
-	// StackTraceElement e = stacktrace[2];// maybe this number needs to be
-	// corrected
-	// System.out.println((errored ? "ERRORED" : "Success") + " JavaFX initializing!
-	// " + e);
-	// }
+	public static void go() {
+		if (latch) {
+			// System.out.println("ERR initializer already started");
+			return;
+		}
+		new Thread() {
+			public void run() {
+				try {
+					gointernal();
+				} catch (Throwable t) {
+					t.printStackTrace();
+					errored = true;
+				}
+			}
+		}.start();
+		try {
+			long start = System.currentTimeMillis();
+			while ((System.currentTimeMillis() - start) < 1000 && latch == false) {
+				Thread.sleep(16);
+			}
+			if (!latch)
+				errored = true;
+		} catch (Throwable e) {
+			e.printStackTrace();
+			errored = true;
+		}
+		StackTraceElement[] stacktrace = Thread.currentThread().getStackTrace();
+		StackTraceElement e = stacktrace[2];// maybe this number needs to be
+
+		System.out.println((errored ? "ERRORED" : "Success") + " JavaFX initializing!" + e);
+	}
 
 }
